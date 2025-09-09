0d0c890c9e1c14bf3174119f25f01253bb11c22d
MauriciOliva <hectoroliva841@gmal.com>
2025-09-09 16:19:02 -0600

    a

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { initServer } from "./config/app.js";
 import { config } from "dotenv";
 import { connect } from "./config/mongo.js";
-import "./src/Agenda/notifier.js"
 
 config();
 connect();
