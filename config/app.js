50e6b0440041b4bea23c02e3e21687d4560104cb
MauriciOliva <hectoroliva841@gmal.com>
2025-09-09 16:50:32 -0600

    eliminacion del * de cors

--- config/app.js
+++ config/app.js
@@ -23,7 +23,6 @@ const config = (app) => {
     };
 
     app.use(cors(corsOptions));
-    app.options('*', cors(corsOptions)); // Habilitar preflight para todas las rutas
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(helmet());
